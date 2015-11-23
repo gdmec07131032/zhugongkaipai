feb620e5803003486bb883794e9991f94ea544e9
gdmec07131032 <592876487@qq.com>
2015-11-23 21:13:44 +0800

    video 1-3

---
+++ Assets/Scriptes/Game/SceneLoading.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneLoading : MonoBehaviour {
+
+    private UISlider mSlider;
+    private UILabel mLabel;
+
+    void Start()
+    {
+        mSlider = transform.Find("Slider").GetComponent<UISlider>();
+        mLabel = transform.Find("Label").GetComponent<UILabel>();
+        mSlider.value = 0.0f;
+        SetLabelInfo(mSlider.value);
+        
+        StartCoroutine(Test());
+    }
+
+    /// <summary>
+    /// 通过异步实现递归，模拟进度条
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Test()
+    {
+        yield return 1;
+        yield return new WaitForSeconds(0.2f);
+        mSlider.value += 0.01f;
+        SetLabelInfo(mSlider.value);
+
+        if (mSlider.value < 1)
+        {
+            StartCoroutine(Test());
+        }
+        else
+        {
+            Debug.Log("加载完成了!");
+        }
+    }
+
+    void SetLabelInfo(float value)
+    {
+        mLabel.text = (value * 100.0f).ToString() + "%";
+    }
+}
