3a0507f15d94fa5b8cd4e6c74d7ba3ff30c2a694
gdmec07131032 <592876487@qq.com>
2015-11-24 23:35:32 +0800

    video 1-5，1-6

---
+++ Assets/Scriptes/Game/SceneMail.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class SceneMail : MonoBehaviour {
+
+    public GameObject mItem;
+
+    private List<GameObject> mItemList;
+
+    void Start()
+    {    
+        mItem = transform.Find("PanelMove/Items/Item").gameObject;
+        ShowItems();
+    }
+
+    /// <summary>
+    /// 显示item列表
+    /// </summary>
+    void ShowItems()
+    {
+        if (mItem == null)
+        {
+            Debug.LogError("Item null");
+        }
+        else
+        {
+            mItemList = new List<GameObject>();
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject item = Instantiate(mItem) as GameObject;
+                item.transform.parent = mItem.transform.parent;
+                item.SetActive(true);
+                item.transform.localScale = Vector3.one;
+                item.transform.localEulerAngles = Vector3.zero;
+                InitItem(item, i);
+                item.name = i.ToString();
+            }
+        }
+    }
+    /// <summary>
+    /// 初始化item
+    /// </summary>
+    /// <param name="item"></param>
+    /// <param name="index"></param>
+    void InitItem(GameObject item, int index)
+    {
+        //设定坐标
+        item.transform.localPosition = new Vector3(0, 135 - index * 90, 0);
+
+        GameObject btnDelete = item.transform.Find("BtnDelete").gameObject;
+
+        UIEventListener listener = UIEventListener.Get(btnDelete);
+        listener.onClick = ClickButton;
+
+        UILabel title = item.transform.Find("Title").GetComponent<UILabel>();
+        UILabel time = item.transform.Find("SendTime").GetComponent<UILabel>();
+        title.text = "李奶奶的来信" + index.ToString();
+        time.text = DateTime.Now.ToString();
+        mItemList.Add(item);
+
+    }
+
+    /// <summary>
+    /// 点击事件
+    /// </summary>
+    /// <param name="click"></param>
+    void ClickButton(GameObject click)
+    {
+        if (click.name.Equals("BtnDelete"))
+        {
+            Debug.Log("点击了" + click.transform.parent.name);
+            mItemList.Remove(click.transform.parent.gameObject);
+            Destroy(click.transform.parent.gameObject);
+            ChangePosition();
+            
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void ChangePosition()
+    {
+        for (int i = 0; i < mItemList.Count; i++)
+        {
+            //GameObject item = mItemList[i];
+            mItemList[i].transform.localPosition = new Vector3(0, 135 - i * 90, 0);
+        }
+    }
+
+}
---
+++ Assets/Scriptes/TestRes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        GameObject obj = ResourcesMgr.GetInstance().CreateGameObject("Game/UI/SceneLoading", false);
+        obj.transform.parent = this.gameObject.transform;
+        obj.transform.localScale = Vector3.one;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
